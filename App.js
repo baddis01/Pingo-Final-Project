473a482f9ba39e2dc428f5fdb7fadf6c4738d004
baddis01 <96215678+baddis01@users.noreply.github.com>
2022-03-21 11:07:37 +0000

    updated dependencies

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./db/config";
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
