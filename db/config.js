707e312a32f10ec1b5cef189424f3bc0f0ede137
baddis01 <96215678+baddis01@users.noreply.github.com>
2022-03-21 12:05:43 +0000

    added db file

---
+++ db/config.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+console.log(process.env.FIREBASEAPIKEY);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZxkbkaR1gzfeuIQbIyvB2wPb0_sEQrXs",
+  authDomain: "pingo-8f9f8.firebaseapp.com",
+  projectId: "pingo-8f9f8",
+  storageBucket: "pingo-8f9f8.appspot.com",
+  messagingSenderId: "293333039104",
+  appId: "1:293333039104:web:da2f3e54ee6fe6b4bb80e5",
+};
+
+export default firebaseConfig;
