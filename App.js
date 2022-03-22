6974c963ad68e021039ed0d5459d548d134912ca
BerniHarris <harrisberni@gmail.com>
2022-03-22 11:07:11 +0000

    add user info to app.js

--- App.js
+++ App.js
@@ -4,16 +4,27 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "./db/config";
 import Pack from "./components/Pack";
 import Packs from "./components/Packs";
+import Login from "./components/Login";
+import { UserContext } from "./contexts/UserContext";
+import { useState } from 'react';
+import UserInfo from "./components/UserInfo";
+
 
 const app = initializeApp(firebaseConfig);
 
 export default function App() {
+const [user, setUser] = useState(null);
+
   return (
+    <UserContext.Provider value={{ user, setUser }}>
     <View style={styles.container}>
-      <Packs />
-      <Pack />
+      <UserInfo/>
+      <Login/>
+      {/* <Packs />
+      <Pack /> */}
       {/* <StatusBar style="auto" /> */}
     </View>
+    </UserContext.Provider>
   );
 }
 
