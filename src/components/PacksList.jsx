b1ab57ddf5a4afb131bfc7be40e17ad2d40af1a0
Duncan <twoontheriver@gmail.com>
2022-03-25 13:20:02 +0000

    get the urls for the images from firebase

--- src/components/PacksList.jsx
+++ src/components/PacksList.jsx
@@ -1,89 +1,99 @@
 import {
-    Text,
-    FlatList,
-    SafeAreaView,
-    StyleSheet,
-    TouchableOpacity,
-    Image,
-    View
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useFonts, BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
-import AppLoading from 'expo-app-loading';
-import React, { useEffect, useState } from 'react';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+  Text,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
+import AppLoading from "expo-app-loading";
+import React, { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const PacksList = ({ packs }) => {
-    const navigation = useNavigation();
-    const [urls, setUrls] = useState([]);
-    useEffect(() => {
-        const func = async () => {
-            const storage = getStorage();
-            const manReference = ref(storage, '/manchester.png');
-            const lonReference = ref(storage, '/london.png')
-            getDownloadURL(manReference, lonReference).then((x) => {
-                setUrls((currentUrl) => {
-                    return [{ manchester: x }]
-                });
-            });
-        };
-        // if (url === undefined) { func() };
-    }, []);
+  const navigation = useNavigation();
+  const [urls, setUrls] = useState({});
+  const [imagesLoading, setImagesLoading] = useState(true);
 
-    let [fontsLoaded] = useFonts({
-        BebasNeue_400Regular,
+  useEffect(() => {
+    if (Object.keys(urls).length > 0) return;
+    setImagesLoading(true);
+    const storage = getStorage();
+    const manReference = ref(storage, "/manchester.png");
+    const lonReference = ref(storage, "/london.png");
+    Promise.all([
+      getDownloadURL(manReference),
+      getDownloadURL(lonReference),
+    ]).then(([manUrl, lonUrl]) => {
+      setImagesLoading(false);
+      setUrls(() => {
+        return { manchester: manUrl, london: lonUrl };
+      });
     });
+  }, []);
 
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    }
-    return (
-        <SafeAreaView>
+  let [fontsLoaded] = useFonts({
+    BebasNeue_400Regular,
+  });
 
-            <FlatList
-                data={packs}
-                keyExtractor={(packs, index) => {
-                    return 'pack' + index;
-                }}
-                renderItem={({ item }) => {
-                    return (<View>
-                        <Image style={styles.imagestyle} source={{ uri: urls[item.data.title.toLowerCase()] }} />
-                        {console.log(urls[item.data.title.toLowerCase()])}
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() =>
-                                navigation.navigate('Pack', {
-                                    itemId: item.id,
-                                })
-                            }
-                        >
-                            <Text style={styles.packs}>{item.data.title}</Text>
-                        </TouchableOpacity>
-                    </View>
-                    );
-                }}
-            />
-        </SafeAreaView>
-    );
+  if (!fontsLoaded || imagesLoading) {
+    return <AppLoading />;
+  }
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={packs}
+        keyExtractor={(packs, index) => {
+          return "pack" + index;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              {/* {console.log(item.data.title, "<title")}
+              {console.log(urls[item.data.image], "<url")}
+              {console.log(urls, "<urls")} */}
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() =>
+                  navigation.navigate("Pack", {
+                    itemId: item.id,
+                  })
+                }
+              >
+                <Text style={styles.packs}>{item.data.title}</Text>
+              </TouchableOpacity>
+              <Image
+                style={styles.imagestyle}
+                source={{ uri: urls[item.data.image] }}
+              />
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {},
-    packs: {
-        fontSize: 50,
-        textAlign: 'center',
-        fontFamily: 'BebasNeue_400Regular',
-    },
-    imagestyle: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'contain',
-        paddingTop: 25,
-        paddingBottom: 100,
-        marginLeft: 170,
-        marginTop: 10,
-    },
+  button: {},
+  packs: {
+    fontSize: 50,
+    textAlign: "center",
+    fontFamily: "BebasNeue_400Regular",
+  },
+  imagestyle: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "contain",
+    paddingTop: 25,
+    paddingBottom: 100,
+    marginLeft: 170,
+    marginTop: 10,
+  },
 });
 
 export default PacksList;
