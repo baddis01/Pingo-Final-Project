bbac2199ca3f6b536fa1fea95401573968cd413b
Adam <adamxgrime@gmail.com>
2022-03-25 12:00:49 +0000

    added map and currently console logging location

--- src/components/PacksList.jsx
+++ src/components/PacksList.jsx
@@ -1,31 +1,83 @@
-import { Text, FlatList, SafeAreaView } from "react-native";
+import {
+  Text,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as Location from "expo-location";
+import MapView from "react-native-maps";
+import { useState, useEffect } from "react";
 
 const PacksList = ({ packs }) => {
-    const navigation = useNavigation();
-    return (
-        <SafeAreaView>
-            <FlatList
-                data={packs}
-                keyExtractor={(packs, index) => {
-                    return "pack" + index;
-                }}
-                renderItem={({ item }) => {
-                    return (
-                        <Text
-                            onPress={() =>
-                                navigation.navigate("Pack", {
-                                    itemId: item.id,
-                                })
-                            }
-                        >
-                            {item.data.title}
-                        </Text>
-                    );
-                }}
-            />
-        </SafeAreaView>
-    );
+  const navigation = useNavigation();
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log(text, "13499wreiub");
+  }
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={packs}
+        keyExtractor={(packs, index) => {
+          return "pack" + index;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <Text
+              onPress={() =>
+                navigation.navigate("Pack", {
+                  itemId: item.id,
+                })
+              }
+            >
+              {item.data.title}
+            </Text>
+          );
+        }}
+      />
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
 
 export default PacksList;
