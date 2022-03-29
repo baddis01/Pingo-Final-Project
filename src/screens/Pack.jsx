a11516e9c57e833a7e792f7efd73d887a5c02ed8
Duncan <twoontheriver@gmail.com>
2022-03-29 11:12:35 +0100

    adds tabbing to pack screen

--- src/screens/Pack.jsx
+++ src/screens/Pack.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import TasksList from "../components/TasksList";
+import UsersList from "../components/UsersList";
+import MapView from "../components/MapView";
 import * as db from "../db";
 import { Text, View, StyleSheet } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
 
 const Pack = () => {
+  const Tab = createBottomTabNavigator();
   const [pack, setPack] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const route = useRoute();
@@ -29,16 +33,32 @@ const Pack = () => {
       </View>
     );
 
+  // return (
+  //   <Tab.Navigator screenOptions={{ headerShown: false }}>
+  //     <Tab.Screen name="Home" component={UsersList} />
+  //     <Tab.Screen name="Settings" component={MapView} />
+  //   </Tab.Navigator>
+  // );
+
   return (
-    <View>
+    <>
       <Text style={styles.packTitle}> {pack.title} </Text>
-      <TasksList
-        style={styles.packTitle}
-        users={pack.users}
-        tasks={pack.tasks}
-        packId={packId}
-      />
-    </View>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Tasks"
+          children={() => (
+            <TasksList
+              style={styles.packTitle}
+              users={pack.users}
+              tasks={pack.tasks}
+              packId={packId}
+            />
+          )}
+        />
+        <Tab.Screen name="Users" component={UsersList} />
+        <Tab.Screen name="Map" component={MapView} />
+      </Tab.Navigator>
+    </>
   );
 };
 
