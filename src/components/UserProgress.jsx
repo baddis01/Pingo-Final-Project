08790f570536669f54c3f3b9e954248e6222e105
Duncan <twoontheriver@gmail.com>
2022-03-30 11:32:28 +0100

    fixes status bar

--- src/components/UserProgress.jsx
+++ src/components/UserProgress.jsx
@@ -2,9 +2,6 @@ import { Text } from "react-native";
 import * as Progress from "react-native-progress";
 
 const UserProgress = ({ user, username, packSize }) => {
-  if (username == "duncan") {
-    console.log(user);
-  }
   let completedTasksCount = Object.keys(user).length;
 
   return (
@@ -12,7 +9,7 @@ const UserProgress = ({ user, username, packSize }) => {
       <Text>
         '{username}' has completed : {completedTasksCount}/{packSize}
       </Text>
-      <Progress.Bar progress={0.3} width={200} />
+      <Progress.Bar progress={completedTasksCount / packSize} width={200} />
     </>
   );
 };
