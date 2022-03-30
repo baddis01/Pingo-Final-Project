cf73e2ecad2fe771aba6a4e37c48e5146bc4a7b5
BerniHarris <harrisberni@gmail.com>
2022-03-30 12:31:22 +0100

    remove title as already on navigation

--- src/screens/Packs.jsx
+++ src/screens/Packs.jsx
@@ -4,45 +4,41 @@ import * as db from "../db";
 import { Text, View, StyleSheet } from "react-native";
 import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
 
-
 const Packs = () => {
-    const [packs, setPacks] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [packs, setPacks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    let [fontsLoaded] = useFonts({
-        BebasNeue_400Regular,
+  let [fontsLoaded] = useFonts({
+    BebasNeue_400Regular,
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    db.getPacks().then((data) => {
+      setPacks(data);
+      setIsLoading(false);
     });
+  }, []);
 
-    useEffect(() => {
-        setIsLoading(true);
-        db.getPacks().then((data) => {
-            setPacks(data);
-            setIsLoading(false);
-        });
-    }, []);
-
-    return (
-        <View>
-            <Text style={styles.packList}>All Packs</Text>
-            <PacksList packs={packs} />
-        </View>
-    );
+  return (
+    <View>
+      <PacksList packs={packs} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    packList: {
-        fontSize: 75,
-        textAlign: 'center',
-        fontFamily: 'BebasNeue_400Regular',
-        marginBottom: 25,
-        color: '#24112F',
-
-    },
-    lottie: {
-        width: 100,
-        height: 100,
-    },
-
-})
+  packList: {
+    fontSize: 75,
+    textAlign: "center",
+    fontFamily: "BebasNeue_400Regular",
+    marginBottom: 25,
+    color: "#24112F",
+  },
+  lottie: {
+    width: 100,
+    height: 100,
+  },
+});
 
 export default Packs;
